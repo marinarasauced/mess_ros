7ef64e3398c74d1e4a18f6a6dc79bb5f84f146fe
Marina Nelson <150466665+marinarasauced@users.noreply.github.com>
2024-06-12 09:12:52 -0400

    Update mess_ros_install_ugv.sh

--- install/mess_ros_install_ugv.sh
+++ install/mess_ros_install_ugv.sh
@@ -4,69 +4,139 @@
 name_ros_distro=noetic 
 user_name=$(whoami)
 
-#Install ROS Noetic using QBotics Labs ros_install_noetic.sh
-cd && wget -c https://raw.githubusercontent.com/qboticslabs/ros_install_noetic/master/ros_install_noetic.sh && chmod +x ./ros_install_noetic.sh && echo "3" | ./ros_install_noetic.sh
-source ~/.bashrc
 
 echo "#######################################################################################################################"
 echo ""
 echo ">>> {Starting MESS-ROS Configuration}"
 echo ""
 echo "#######################################################################################################################"
-echo ">>> {Step 1: Create MESS-ROS Workspace}"
+echo ">>> {Step 1: ROS Noetic Installation}"
 echo ""
-
-#Create ROS workspace for MESS supporting packages.
-echo ">>> {Making mess_ros workspace}"
+echo ">>> {Task: Setting up sources.list}"
 echo ""
-
-source ~/.bashrc
-cd && mkdir mess_ros && cd mess_ros && mkdir src && catkin_make
-echo "source ~/mess_ros/devel/setup.bash" >> /home/$user_name/.bashrc
-source ~/.bashrc
-
-echo ">>> {Done: Created mess_ros Workspace}"
+#
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+#
+echo ""
+echo ">>> {Done: Set up sources.list}"
+echo ""
+echo ">>> {Task: Setting up keys}"
+echo ""
+#
+sudo apt install curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+#
+echo ""
+echo ">>> {Done: Set up keys}"
+echo ""
+echo ">>> {Task: Installing ROS Noetic base}"
+echo ""
+#
+sudo apt update
+sudo apt install ros-noetic-ros-base
+#
+echo ""
+echo ">>> {Done: Installed ROS Noetic base}"
+echo ""
+echo ">>> {Task: Setting up environment}"
+echo ""
+#
+source /opt/ros/noetic/setup.bash
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+#
+echo ""
+echo ">>> {Done: Set up environment}"
+echo ""
+echo ">>> {Task: Installing dependencies}"
+echo ""
+#
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+#
+echo ""
+echo ">>> {Done: Installed dependencies}"
+echo ""
+echo ">>> {Task: Initializing dependencies}"
+echo ""
+#
+sudo apt install python3-rosdep
+sudo rosdep init
+rosdep update
+#
+echo ""
+echo ">>> {Done: Initialized dependencies}"
+echo ""
 echo ""
 echo "#######################################################################################################################"
-echo ">>> {Step 2: Installing TurtleBot3 Packages}"
+echo ">>> {Step 2: MESS-ROS Workspace Configuration}"
 echo ""
-
+echo ">>> {Task: Creating mess_ros workspace}"
+echo ""
+#
+source /opt/ros/noetic/setup.bash
+cd 
+mkdir mess_ros
+cd ~/mess_ros
+mkdir src
+catkin_make
+source ~/mess_ros/devel/setup.bash
+echo "source ~/mess_ros/devel/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+#
+echo ""
+echo ">>> {Done: Created mess_ros workspace}"
+echo ""
+echo ""
+echo "#######################################################################################################################"
+echo ">>> {Step 3: Installing TurtleBot3 Packages}"
+echo ""
+echo ">>> {Task: Adding turtlebot3 packages to mess_ros workspace}"
+echo ""
+#
 sudo apt update
 sudo apt install libudev-dev
-cd ~/mess_ros/src && git clone https://github.com/ROBOTIS-GIT/turtlebot3.git
-cd ~/mess_ros/src && git clone https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
-cd ~/mess_ros/src && git clone -b develop https://github.com/ROBOTIS-GIT/ld08_driver.git
-cd ~/mess_ros/src/turtlebot3 && git pull
-cd ~/mess_ros/src/turtlebot3 && rm -r turtlebot3_description/ turtlebot3_teleop/ turtlebot3_navigation/ turtlebot3_slam/ turtlebot3_example/
-
+cd ~/mess_ros/src
+git clone https://github.com/ROBOTIS-GIT/turtlebot3.git
+git clone https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
+git clone -b develop https://github.com/ROBOTIS-GIT/ld08_driver.git
+cd ~/mess_ros/src/turtlebot3
+git pull
+rm -r turtlebot3_description/ turtlebot3_teleop/ turtlebot3_navigation/ turtlebot3_slam/ turtlebot3_example/
+#
 echo ""
-echo ">>> {Done: Added turtlebot3 packages}"
+echo ">>> {Done: Added turtlebot3 packages to mess_ros workspace}"
+echo ""
 echo ""
 echo "#######################################################################################################################"
-echo ">>> {Step 3: Installing MESS-ROS Packages}"
+echo ">>> {Step 4: Installing MESS-ROS Packages}"
 echo ""
-
-#Clone mess_ros packages from github repo and remove unnecessary ones.
-cd ~/mess_ros/src && git clone https://github.com/marinarasauced/mess_ros.git && cd mess_ros
-cd ~/mess_ros/src/mess_ros && rm -r install/ messop_uav/
-
+echo ">>> {Task: Added mess_ros packages to mess_ros workspace}"
 echo ""
-echo ">>> {Done: Added mess_ros packages}"
+#
+cd ~/mess_ros/src
+git clone https://github.com/marinarasauced/mess_ros.git
+cd ~/mess_ros/src/mess_ros
+rm -r install/ messop_uav/
+#
 echo ""
-echo ">>> {Cloning modified files to turtlebot3_bringup}"
+echo ">>> {Done: Added mess_ros packages to mess_ros workspace}"
 echo ""
-
-#Replace turtlebot3_bringup files.
+echo ">>> {Task: Updating turtlebot3_bringup files}"
+echo ""
+#
 sudo apt-get install libjsoncpp-dev
-cd ~/mess_ros/src/turtlebot3 && rm -r turtlebot3_bringup/CMakeLists.txt turtlebot3_bringup/src/turtlebot3_diagnostics.cpp
+cd ~/mess_ros/src/turtlebot3/turtlebot3_bringup
+rm -r CMakeLists.txt src/turtlebot3_diagnostics.cpp
 cp ~/mess_ros/src/mess_ros/messop_ugv/config/move2turtlebot3_bringup/CMakeLists.txt ~/mess_ros/src/turtlebot3/turtlebot3_bringup
 cp ~/mess_ros/src/mess_ros/messop_ugv/config/move2turtlebot3_bringup/turtlebot3_diagnostics.cpp ~/mess_ros/src/turtlebot3/turtlebot3_bringup/src
-
+#
 echo ""
-echo ">>> {Done: Replaced CMakeLists.txt and turtlebot3_diagnostics.cpp}"
+echo ">>> {Done: Updated turtlebot3_bringup files}"
+echo ""
 echo ""
 echo "#######################################################################################################################"
-echo ">>> {Step 4: Setting up OpenCR, you pick which turtlebot3 model to configure for}"
+echo ">>> {Step 5: Setting up OpenCR}"
+echo ""
+echo ">>> {User: Enter desired turtlebot3 model for configuration}"
 echo ""
 echo "     [1. Burger]"
 echo ""
@@ -74,11 +144,9 @@ echo "     [2. Waffle]"
 echo ""
 echo "     [3. WafflePi]"
 echo ""
-
-#Assigning default value as 1: Burger
-read -p "Enter your turtlebot3 model (Default is 1):" answer 
-
-case "$answer" in
+#
+read -p "Enter your turtlebot3 model (Default is 1):" answer1 
+case "$answer1" in
   1)
     firmware_model="burger"
     turtlebot3_model="burger"
@@ -96,33 +164,35 @@ case "$answer" in
     turtlebot3_model="burger"
     ;;
 esac
-
+#
+echo ""
+echo ">>> {Task: Configuring turtlebot3 OpenCR for selected model}"
+echo ""
+#
 sudo dpkg --add-architecture armhf
 sudo apt-get update
 sudo apt-get install libc6:armhf
-
 export OPENCR_PORT=/dev/ttyACM0
 export OPENCR_MODEL=${firmware_model}_noetic
-cd && rm -rf ./opencr_update.tar.bz2
-
-cd && wget https://github.com/ROBOTIS-GIT/OpenCR-Binaries/raw/master/turtlebot3/ROS1/latest/opencr_update.tar.bz2 
+cd
+rm -rf ./opencr_update.tar.bz2
+wget https://github.com/ROBOTIS-GIT/OpenCR-Binaries/raw/master/turtlebot3/ROS1/latest/opencr_update.tar.bz2 
 tar -xvf opencr_update.tar.bz2 
-
 cd ./opencr_update
 ./update.sh $OPENCR_PORT $OPENCR_MODEL.opencr
-
+#
 echo ""
-echo ">>> {Done: Updated OpenCR Firmware, you pick which lidar model to configure for}"
+echo ">>> {Task: Updated OpenCR firmware for selected model}"
+echo ""
+echo ">>> {User: Enter desired lds model for configuration}"
 echo ""
 echo "     [1. LDS-01]"
 echo ""
 echo "     [2. LDS-02]"
 echo ""
-
-#Assigning default value as 1: LDS-02
-read -p "Enter your lidar model (Default is 2):" answer 
-
-case "$answer" in
+#
+read -p "Enter your lidar model (Default is 2):" answer2
+case "$answer2" in
   1)
     lidar_model="LDS-01"
     ;;
@@ -133,24 +203,31 @@ case "$answer" in
     lidar_model="LDS-02"
     ;;
 esac
-
+#
+echo ""
+echo ">>> {Task: Configuring environment for selected turtlebot3 and lidar models}"
+echo ""
+#
 echo "export TURTLEBOT3_MODEL=${turtlebot3_model}" >> /home/$user_name/.bashrc
 echo "export LDS_MODEL=${lidar_model}" >> /home/$user_name/.bashrc
-
+#
 echo ""
-echo ">>> {Done: Configured turtlebot3 and lidar models}"
+echo ">>> {Done: Configured environment}"
+echo ""
 echo ""
 echo "#######################################################################################################################"
-echo ">>> {Step 5: Configuring Netplan, you enter the network SSID and password}"
+echo ">>> {Step 6: Configuring Netplan}"
 echo ""
-
+echo ">>> {User: Enter you network SSID and password}"
+echo ""
+#
 read -p "Enter your network SSID:" network_ssid
 read -p "Enter your network password:" network_pw
-
+#
 echo ""
-echo ">>> {Writing netplan file}"
+echo ">>> {Task: Writing netplan file}"
 echo ""
-
+#
 sudo apt install net-tools
 echo "network:" >> /etc/netplan/01-network-manager-all.yaml
 ehco "  version: 2" >> /etc/netplan/01-network-manager-all.yaml
@@ -162,20 +239,28 @@ echo "      access-points:" >> /etc/netplan/01-network-manager-all.yaml
 echo "        ${network_ssid}:" >> /etc/netplan/01-network-manager-all.yaml
 echo "          password: ${network_pw}" >> /etc/netplan/01-network-manager-all.yaml
 netplan apply
-
+#
 echo ""
-echo ">>> {Done: Netplan written and applied}"
+echo ">>> {Done: Wrote and applied netplan file}"
+echo ""
 echo ""
 echo "#######################################################################################################################"
 echo ">>> {Step 6: Finalizing ROS Configuration}"
 echo ""
-
+echo ">>> {Task: Finalizing environment configuration}"
+echo ""
+#
+sleep 5
 ip_address=$(ifconfig wlan0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 cd mess_ros && catkin_make
 echo "export ROS_MASTER_URI=http://192.168.0.229:11311" >> /home/$user_name/.bashrc
 echo "export ROS_HOSTNAME=${ip_address}" >> /home/$user_name/.bashrc
-
+#
 echo ""
-echo ">>> {Done: MESS-ROS configured}"
+echo ">>> {Done: Finalized environment configuration}"
+echo ""
+echo "#######################################################################################################################"
+echo ""
+echo ">>> {Ended MESS-ROS Configuration}"
 echo ""
 echo "#######################################################################################################################"
