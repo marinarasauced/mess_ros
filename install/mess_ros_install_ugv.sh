ab84e09d6c07e68cf41e3cb9db25f7f374cc1e59
Marina Nelson <150466665+marinarasauced@users.noreply.github.com>
2024-06-12 22:44:31 -0400

    Update mess_ros_install_ugv.sh

--- install/mess_ros_install_ugv.sh
+++ install/mess_ros_install_ugv.sh
@@ -107,8 +107,8 @@ echo ""
 echo ">>> {Task: Setting up environment}"
 echo ""
 #
-echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
-source ~/.bashrc
+echo "source /opt/ros/noetic/setup.bash" >> /home/$user_name/.bashrc
+source /home/$user_name/.bashrc
 #
 echo ""
 echo ">>> {Done: Set up environment}"
@@ -139,11 +139,11 @@ echo ""
 #
 cd 
 mkdir mess_ros
-cd ~/mess_ros
+cd /home/$user_name/mess_ros
 mkdir src
 catkin_make
-echo "source ~/mess_ros/devel/setup.bash" >> ~/.bashrc
-source ~/.bashrc
+echo "source /home/$user_name/mess_ros/devel/setup.bash" >> /home/$user_name/.bashrc
+source /home/$user_name/.bashrc
 #
 echo ""
 echo ">>> {Done: Created mess_ros workspace}"
@@ -157,11 +157,11 @@ echo ""
 #
 sudo apt update
 sudo apt install -y libudev-dev
-cd ~/mess_ros/src
+cd /home/$user_name/mess_ros/src
 git clone https://github.com/ROBOTIS-GIT/turtlebot3.git
 git clone https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
 git clone -b develop https://github.com/ROBOTIS-GIT/ld08_driver.git
-cd ~/mess_ros/src/turtlebot3
+cd /home/$user_name/mess_ros/src/turtlebot3
 git pull
 rm -r turtlebot3_description/ turtlebot3_teleop/ turtlebot3_navigation/ turtlebot3_slam/ turtlebot3_example/
 #
@@ -175,9 +175,9 @@ echo ""
 echo ">>> {Task: Added mess_ros packages to mess_ros workspace}"
 echo ""
 #
-cd ~/mess_ros/src
+cd /home/$user_name/mess_ros/src
 git clone https://github.com/marinarasauced/mess_ros.git
-cd ~/mess_ros/src/mess_ros
+cd /home/$user_name/mess_ros/src/mess_ros
 rm -r install/ messop_uav/
 #
 echo ""
@@ -187,10 +187,10 @@ echo ">>> {Task: Updating turtlebot3_bringup files}"
 echo ""
 #
 sudo apt-get install -y libjsoncpp-dev
-cd ~/mess_ros/src/turtlebot3/turtlebot3_bringup
+cd /home/$user_name/mess_ros/src/turtlebot3/turtlebot3_bringup
 rm -r CMakeLists.txt src/turtlebot3_diagnostics.cpp
-cp ~/mess_ros/src/mess_ros/messop_ugv/config/move2turtlebot3_bringup/CMakeLists.txt ~/mess_ros/src/turtlebot3/turtlebot3_bringup
-cp ~/mess_ros/src/mess_ros/messop_ugv/config/move2turtlebot3_bringup/turtlebot3_diagnostics.cpp ~/mess_ros/src/turtlebot3/turtlebot3_bringup/src
+cp /home/$user_name/mess_ros/src/mess_ros/messop_ugv/config/move2turtlebot3_bringup/CMakeLists.txt /home/$user_name/mess_ros/src/turtlebot3/turtlebot3_bringup
+cp /home/$user_name/mess_ros/src/mess_ros/messop_ugv/config/move2turtlebot3_bringup/turtlebot3_diagnostics.cpp /home/$user_name/mess_ros/src/turtlebot3/turtlebot3_bringup/src
 #
 echo ""
 echo ">>> {Done: Updated turtlebot3_bringup files}"
@@ -257,10 +257,10 @@ echo ""
 #
 sleep 5
 ip_address=$(ifconfig wlan0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
-cd ~/mess_ros && catkin_make
+cd /home/$user_name/mess_ros && catkin_make
 echo "export ROS_MASTER_URI=http://192.168.0.229:11311" >> /home/$user_name/.bashrc
 echo "export ROS_HOSTNAME=${ip_address}" >> /home/$user_name/.bashrc
-source ~/.bashrc
+source /home/$user_name/.bashrc
 #
 echo ""
 echo ">>> {Done: Finalized environment configuration}"
