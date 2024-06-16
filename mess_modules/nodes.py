69247904564467489a3bb6668b931b29a71d5ba5
Marina Nelson <150466665+marinarasauced@users.noreply.github.com>
2024-06-16 11:49:29 -0400

    Delete mess_modules directory

--- mess_modules/nodes.py
+++
@@ -1,9 +0,0 @@
-
-
-class UGVNode():
-    def __init__(self, package, file_name, node_name):
-        self.pkg = package
-        self.type = file_name
-        self.name = node_name
-        self.node = f"    <node pkg=\"{self.pkg}\" type=\"{self.type}\" name=\"$(arg ugv_name)_{self.name}\" output=\"screen\"/>"
-
