fbb3e38f855837f79993f3e8bdfc80efb1581260
Prakash Poudel <131793478+prakashpoudel2014@users.noreply.github.com>
2024-06-20 15:36:52 -0400

    small bugs

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # mess_ros
 ROS1 Noetic packages supporting the ACE Lab's Modular Experiment Software System.
+rr
--- experiments/src/demo1.py
+++ experiments/src/demo1.py
@@ -21,7 +21,7 @@ def run_experiment(burger2):
     
     points = np.array([
         [1.0, 2.0],
-        [1.0, 0.0],
+        [2.0, 0.0],
         [0.0, 0.0]
     ])
     counter = 0
